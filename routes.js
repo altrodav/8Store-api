5338aa2b8b7e2ffb7f7a09f97bee21d084567d0f
Ashutosh Kumar <ashu28jan2003@gmail.com>
2024-09-22 16:49:47 +0530

    update

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const route = express.Router();
+const routes = express.Router();
 
 const User = require("./routes/userRoutes")
 
-route.use("/user",User); // Starting endpoint-> /api/user
+routes.use("/user",User); // Starting endpoint-> /api/user
 
-module.exports = route;
+module.exports = routes;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const userData = require("../modules/userSchema");
 const route = express.Router();
