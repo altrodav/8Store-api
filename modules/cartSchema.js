94d3482dae2ae33424f43ca864242435aba6a2b0
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-09-23 13:31:48 +0530

    fix: added color and size schema and route

--- modules/cartSchema.js
+++ modules/cartSchema.js
@@ -1,22 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const cartProductSchema = new Schema({
+const cartProductSchema = new Schema(
+  {
     userId: {
-        type: String, // Reference to the User collection
-        required: true
+      type: String, // Reference to the User collection
+      required: true,
     },
     productId: {
-        type: String, // Reference to the Product collection
-        required: true
+      type: String, // Reference to the Product collection
+      required: true,
     },
     quantity: {
-        type: Number,
-        required: true,
-        min: 1, // Ensure the quantity is at least 1
-        default: 1
-    }
-}, { timestamps: true, versionKey: false});
+      type: Number,
+      required: true,
+      min: 1, // Ensure the quantity is at least 1
+      default: 1,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
 
-module.exports = mongoose.model('CartProduct', cartProductSchema);
+module.exports = mongoose.model("CartProduct", cartProductSchema);
--- routes/cartSchema.js
+++ routes/cartSchema.js
@@ -4,66 +4,67 @@ const route = express.Router();
 
 // The parent path of all is "*host-url*/api/cartData/*next-routes*"
 
-route.get("/getUserCart",async(req,res)=>{
-    try{
-
-        const Id = req.query.Id;
-
-        const result = await cartData.findOne({userId:Id});
-        res.status(200).json({cart:result,success:true});
-
-    }
-    catch(error){
-        res.status(500).json({success:false});
-    }
-});
-
-route.get('/getAllCarts',async(req,res)=>{
-    try{ 
-        const result = await cartData.find({});
-
-        res.status(200).json({cart:result,success:true});
-    }
-    catch(error){
-        res.status(500).json({message:error.message,success:false});
-    }
-});
-
-route.post("/addCart",async(req,res)=>{
-    try{
-        const cart = await cartData.create({
-          userId:req.body.userId,
-          productId:req.body.productId,
-          quantity:req.body.quantity,
-        });
-       res.status(200).json({cart:cart,success:true});
-    }
-    catch(error){
-        res.status(400).json({success:false,message: error.message});
-    }
-});
-
-route.put("/updateCart",async(req,res)=>{
-
+route.get("/getUserCart", async (req, res) => {
+  try {
     const Id = req.query.Id;
-    try{
-       await cartData.updateOne({_id:Id},{
-            quantity:req.body.quantity,
-        });
-        res.status(200).json({success:true});
-    }catch(error){
-        res.status(500).json({success:false,message:error.message});
-    }
+
+    const result = await cartData.findOne({ userId: Id });
+    res.status(200).json({ cart: result, success: true });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
 });
 
-route.delete("/deleteCart",async(req,res)=>{
-    try{
-        const Id = req.query.Id;
-        await cartData.deleteOne({_id:Id});
-        res.status(200).json({success:true});
-    }catch(error){
-        res.status(500).json({success:false,message:error.message});
-    }
+route.get("/getAllCarts", async (req, res) => {
+  try {
+    const result = await cartData.find({});
+
+    res.status(200).json({ cart: result, success: true });
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+});
+
+route.post("/addCart", async (req, res) => {
+  try {
+    const cart = await cartData.create({
+      userId: req.body.userId,
+      productId: req.body.productId,
+      quantity: req.body.quantity,
+      size: req.body.size,
+      color: req.body.color,
+    });
+    res.status(200).json({ cart: cart, success: true });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
+
+route.put("/updateCart", async (req, res) => {
+  const Id = req.query.Id;
+  try {
+    await cartData.updateOne(
+      { _id: Id },
+      {
+        quantity: req.body.quantity,
+        size: req.body.size,
+        color: req.body.color,
+      }
+    );
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+route.delete("/deleteCart", async (req, res) => {
+  try {
+    const Id = req.query.Id;
+    await cartData.deleteOne({ _id: Id });
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 });
 
 module.exports = route;
