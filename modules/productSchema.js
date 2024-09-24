b5bb3dc206a742c2d679d0053a0975d547d8b234
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-09-24 16:12:23 +0530

    fix: change colors props to array of string

--- modules/productSchema.js
+++ modules/productSchema.js
@@ -1,65 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     category: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     subCategory: {
-        type: String,
+      type: String,
     },
-    assets: [{
+    assets: [
+      {
         type: {
-            type: String, // restrict to 'image' or 'video'
-            required: true,
+          type: String, // restrict to 'image' or 'video'
+          required: true,
         },
         url: {
-            type: String,
-            required: true,
-        }
-    }],
-    colors: [{
-        colorName: {
-            type: String,
-            required: true,
+          type: String,
+          required: true,
         },
-        colorCode: {
-            type: String,
-            required: true,
-        },
-        isAvailable: {
-            type: Boolean,
-            default: true, // default value if not provided
-        }
-    }],
-    sizes: [{
+      },
+    ],
+    colors: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    sizes: [
+      {
         sizeName: {
-            type: String,
-            required: true,
+          type: String,
+          required: true,
         },
         weightRange: {
-            type: String,
-            required: true,
+          type: String,
+          required: true,
         },
-        isAvailable: {
-            type: Boolean,
-            default: true, // default value if not provided
-        }
-    }],
+      },
+    ],
     price: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     description: {
-        type: String,
-    }
-}, { 
-    timestamps: true ,
-    versionKey: false
-});
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
-module.exports = mongoose.model('product', productSchema);
+module.exports = mongoose.model("product", productSchema);
